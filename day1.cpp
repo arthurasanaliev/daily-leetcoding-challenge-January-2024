9e1aa37bbfe340b336394d5896ebf51071778980
Arthur Asanaliev <arthis1707@gmail.com>
2024-01-01 08:32:43 +0600

    day 1 commit

---
+++ day1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size(), m = s.size();
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            if (s[j] >= g[i]) i++;
+            j++;
+        }
+
+        return i;
+    }
+};
