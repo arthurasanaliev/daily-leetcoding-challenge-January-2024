a016bc11edbe0c60ee3665a14250f7a75482d8e5
Arthur Asanaliev <arthis1707@gmail.com>
2024-01-08 13:18:34 +0600

    added day 8

---
+++ src/day8.cpp
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/range-sum-of-bst/
+
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        if (root->val > high) {
+            return rangeSumBST(root->left, low, high);
+        }
+        if (root->val < low) {
+            return rangeSumBST(root->right, low, high);
+        }
+        return (
+            root->val + 
+            rangeSumBST(root->left, low, high) + 
+            rangeSumBST(root->right, low, high)
+        );
+    }
+};
